4d0bb0d83de281cc3a97b1536aafd1634b901a95
Alex <skitzky@yandex.ru>
2024-11-21 14:52:17 +0300

    Revert "Correct message"
    This reverts commit 4bf7c66a5cd07a2296a720067237cb6d9eaf1f27.

--- src/post_message.py
+++ src/post_message.py
@@ -10,20 +10,13 @@ connected_users = {}
 async def get_updates(websocket: WebSocket, room_id: str, user: User = Depends()):
     """Вебсокет для получения сообщений"""
     await websocket.accept()
-
     if room_id not in connected_users:
         connected_users[room_id] = []
-
-
-    if len(connected_users[room_id]) >= 2:
-        await websocket.close()
-        return
-
     connected_users[room_id].append(websocket)
 
     try:
         while True:
-            message = await router.consume("message")
+            message = await router.consume("messages")
             if message['room_id'] == room_id:
                 for conn in connected_users[room_id]:
                     await conn.send_json({"user": message['user'], "content": message['content']})
